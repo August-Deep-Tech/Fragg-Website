561567b2caa2c99a36562f0b5f5b28dbf5508ee8
Martins <martins.ikpe@stu.cu.edu.ng>
2024-07-07 16:19:59 +0100

    Landing page Updates

--- src/pageSections/HeroSection.tsx
+++ src/pageSections/HeroSection.tsx
@@ -9,24 +9,24 @@ interface IProps {
 }
 const HeroSection = ({ bgImage }: IProps) => {
     return (
-        <div className={`${bgImage} text-white ${redHatDisplay.className} flex items-end pb-[44px] px-[80px]`}>
+        <div className={`${bgImage} text-white ${redHatDisplay.className} flex items-end pb-[44px] px-[20px] xl:px-[80px]`}>
             <div>
-                <h1 className='text-[96px] leading-[105px]'>Accelerate access <br /> to Inclusive Finance</h1>
-                <div className='flex justify-between pt-[20px]'>
+                <h1 className='text-[32px] leading-[43px] lg:text-[64px] lg:leading-[75px] xl:text-[96px] xl:leading-[105px]'>Accelerate access <br /> to Inclusive Finance</h1>
+                <div className='flex flex-col-reverse gap-[30px] lg:gap-0 lg:flex-row justify-between pt-[20px]'>
                     <div className='flex gap-[10px] w-[100%]'>
                         <Link href="fragg-nigeria/services">
                             <Button
                                 label={"Start Investing"}
                                 type="submit"
                                 iconLeft={<Image src="/assets/fragginvest-logo-small.svg" width={20} height={20} alt="" />}
-                                className="bg-white py-[12px] text-[18px] px-[24px] rounded-[28px] text-black font-semibold w-fit"
+                                className="bg-white py-[12px] text-[14px] sm:text-[18px] px-[24px] rounded-[28px] text-black font-semibold w-fit"
                             /></Link>
                         <Link href="fragg-nigeria/services">
                             <Button
                                 label={"Contact Us"}
                                 type="submit"
                                 iconLeft={<Mail />}
-                                className="bg-transparent border border-white py-[12px] text-[18px] px-[24px] rounded-[28px] text-white font-semibold w-fit"
+                                className="bg-transparent border border-white py-[12px] text-[14px] sm:text-[18px] px-[24px] rounded-[28px] text-white font-semibold w-fit"
                             /></Link>
                     </div>
                     <div className='w-[100%]'>
