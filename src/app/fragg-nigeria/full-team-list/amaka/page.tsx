ec83bc1426f49aab8a679d89277b1f2cf20e1715
Jaachi Okafor <okaforjaachi@gmail.com>
2024-07-07 21:16:10 +0100

    created team page

---
+++ src/app/fragg-nigeria/full-team-list/amaka/page.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Image from "next/image";
+
+const Amaka = () => {
+  return (
+    <div className="container mx-auto px-4 md:px-0 py-20 flex gap-8">
+      {/* image and email */}
+      <div>
+        <div className="w-full">
+          <Image
+            src="/assets/png/team/amaka.png"
+            height={340}
+            width={280}
+            alt="Photo of Amaka"
+          />
+        </div>
+        <div className="text-redish-20 text-center p-4">
+          <a
+            href="mailto:a.unije@fragginvest.com"
+            className="inline-block font-medium "
+          >
+            a.unije@fragginvest.com
+          </a>
+        </div>
+      </div>
+      {/* writeup */}
+      <div className="flex flex-col flex-1 gap-3">
+        <div className="mb-6">
+          <h2 className="font-bold text-4xl text-[#18181B]">Amaka Onyechi</h2>
+          <p className="text-greyish-10 text-2xl">
+            Administrative & Finance Officer
+          </p>
+        </div>
+
+        <p className="text-greyish-10">
+          Amaka Onyechi stands out as a Finance and Admin Officer, boasting 8
+          years of multifaceted experience in the realms of administrative and
+          financial management, human resources, research, data management,
+          customer experience, and digital marketing. Her distinguished
+          contributions to the FRAGG Impact SME Fund 1 and the FRAGG
+          Growth-Impact Accelerator Programmes underscore her expertise in
+          investment strategy and a deep-seated commitment to nurturing startup
+          ventures across West Africa.
+        </p>
+        <p className="text-greyish-10">
+          Amaka's academic foundation, with a BSc in Public Administration from
+          Nnamdi Azikiwe University, Awka Nigeria has been instrumental in her
+          professional development. This background, combined with her expansive
+          role at Fragg Investment Management Ltd, plays a crucial role in
+          advancing our strategic objectives and fostering organizational
+          growth. Amaka's blend of skills and experience not only enhances our
+          operational efficiency but also enriches our firm's culture of
+          innovation and impact-driven investment strategy.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Amaka;
---
+++ src/app/fragg-nigeria/full-team-list/maurice/page.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Image from "next/image";
+
+const Maurice = () => {
+  return (
+    <div className="container mx-auto px-4 md:px-0 py-20 flex gap-8">
+      {/* image and email */}
+      <div>
+        <div className="w-full">
+          <Image
+            src="/assets/png/team/maurice.png"
+            height={340}
+            width={280}
+            alt="Photo of Maurice"
+          />
+        </div>
+        <div className="text-redish-20 text-center p-4">
+          <a
+            href="mailto:m.dinkelbach@fragginvest.com"
+            className="inline-block font-medium "
+          >
+            m.dinkelbach@fragginvest.com
+          </a>
+        </div>
+      </div>
+      {/* writeup */}
+      <div className="flex flex-col flex-1 gap-3">
+        <div className="mb-6">
+          <h2 className="font-bold text-4xl text-[#18181B]">
+            Maurice Dinkelbach
+          </h2>
+          <p className="text-greyish-10 text-2xl">Investment Officer</p>
+        </div>
+
+        <p className="text-greyish-10">
+          Maurice Dinkelbach stands out as an Investment Officer with a distinct
+          blend of entrepreneurship, and advisory expertise across Europe and
+          Latam. His five-year journey of supporting small businesses and
+          startups has been marked by a passion for building enterprises from
+          the foundation up, offering critical investment advisory and readiness
+          services. His pivotal three-year tenure in Mexico at Fragg-Invest GmbH
+          showcased Maurice's exceptional capabilities in deal origination,
+          forging strategic partnerships, conducting due diligence, and closing
+          investments. This period underscored his commitment to enhancing the
+          start-up ecosystem's growth and sustainability, solidifying his role
+          as an integral part of our investment team.
+        </p>
+        <p className="text-greyish-10">
+          Maurice's approach is characterized by a global perspective paired
+          with a nuanced understanding of local market intricacies. This unique
+          combination allows him to uncover and leverage unparalleled investment
+          opportunities. His efforts in nurturing emerging companies
+          significantly propel their growth, diversifying and strengthening our
+          investment portfolio.
+        </p>
+        <p className="text-greyish-10">
+          In his role, Maurice employs his vast entrepreneurial background and
+          sharp investment insights to advance our objective of empowering
+          businesses and sparking innovation worldwide. His academic Degree in
+          Business Administration further enriches his professional pursuits,
+          offering a robust theoretical underpinning to his practical
+          achievements.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Maurice;
