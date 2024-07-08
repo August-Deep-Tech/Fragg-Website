b867538dd927a0b5b3b804190dc0989a97a9fd85
Martins <martins.ikpe@stu.cu.edu.ng>
2024-07-08 05:22:34 +0100

    Landing germany

--- src/app/fragg-gmbh/page.tsx
+++ src/app/fragg-gmbh/page.tsx
@@ -1,8 +1,42 @@
+import BecomeanInvestor from '@/pageSections/BecomeanInvestor'
+import ConnectionSection from '@/pageSections/ConnectionSection'
+import FraggInvestmentManagement from '@/pageSections/FraggInvestment'
+import Group from '@/pageSections/Group'
+import GroupContact from '@/pageSections/GroupContact'
+import GrowWithFragg from '@/pageSections/GrowWithFragg'
+import HeroSectionGmbh from '@/pageSections/HeroSectionGmbh'
+import PriorityStrategy from '@/pageSections/PriorityStrategy'
+import WhatWeOffer from '@/pageSections/WhatWeOffer'
 import React from 'react'
 
 const Landing = () => {
   return (
-    <div>Landing Germany</div>
+    <div>
+      <HeroSectionGmbh />
+      <ConnectionSection />
+      <FraggInvestmentManagement
+        Data={[
+          {
+            title: "FRAGG-INVEST GmbH",
+            subText: "FRAGG-INVEST GmbH is an impact investment manager and advisor committed to mobilizing and managing funds with a strong focus on sustainable investments aligned with the United Nations Sustainable Development Goals (SDGs).",
+          },
+          {
+            title: "Our Target",
+            subText: "With headquarters in Germany, FRAGG-INVEST GmbH specializes in impact investment advisory services, fund mobilization, project development, and fund management. With its commitment to sustainable finance, expertise in impact investing, and extensive network and partnerships, FRAGG-INVEST GmbH is dedicated to driving positive social, environmental, and financial impact across its target regions.",
+          },
+          {
+            title: 'Our Services',
+            subText: "FRAGG-INVEST GmbH provides a range of services to support startups, SMEs, and impact projects in achieving their growth and impact objectives. With expertise in investment readiness, accelerator programs, capital raising, and business development, the firm is dedicated to fostering sustainable entrepreneurship and driving positive social, environmental, and financial outcomes.",
+          },
+        ]
+        } />
+      <WhatWeOffer />
+      <PriorityStrategy />
+      <GrowWithFragg />
+      <Group text="Join the network of FRAGG Group, which includes FRAGG Investment Management in Nigeria, FRAGG Invest-GmbH in Germany, and FRAGG-Invest SÀRL in Luxembourg, providing customized investment solutions across borders." />
+      <GroupContact image="map-germany" />
+      <BecomeanInvestor />
+    </div>
   )
 }
 
--- src/pageSections/FraggInvestment.tsx
+++ src/pageSections/FraggInvestment.tsx
@@ -2,26 +2,14 @@ import { redHatDisplay } from '@/styles/font';
 import { Earth, Goal } from 'lucide-react';
 import Image from 'next/image';
 
-const props = [
-    {
-        backgroundUrl: 'bg-[url("/assets/webp/fragg-nigeria.webp")]',
-        title: "FRAGG Investment Management, Nigeria",
-        linkHref: "/fragg-nigeria/about",
-    },
-    {
-        backgroundUrl: 'bg-[url("/assets/webp/fragg-gmbh.webp")]',
-        title: "FRAGG-Invest GmbH, Germany",
-        linkHref: "/fragg-gmbh/about",
-    },
-    {
-        backgroundUrl: 'bg-[url("/assets/webp/fragg-sarl.webp")]',
-        title: "FRAGG-Invest GP Sàrl, Luxembourg",
-        linkHref: "/fragg-sarl/about",
-    },
-];
+interface Iinvestment {
+    Data: {
+        title: string,
+        subText: string
+    }[]
+}
 
-
-const FraggInvestmentManagement = () => {
+const FraggInvestmentManagement = ({Data}: Iinvestment) => {
     return (
         <div className={`${redHatDisplay.className}`}>
             <div className="pb-[40px] pt-[40px]">
@@ -36,9 +24,9 @@ const FraggInvestmentManagement = () => {
                 <section className="h-[400px] text-white bg-[url('/assets/svg/home/investOne.svg')] px-[24px] py-[40px] flex items-end">
                     <div>
                         <Image src="/assets/fragginvest-logo-small-white.svg" width={30} height={30} alt="" />
-                        <h2 className="text-2xl font-semibold mb-4 pt-[20px]">FRAGG Investment Management, Nigeria</h2>
+                        <h2 className="text-2xl font-semibold mb-4 pt-[20px]">{Data[0].title}</h2>
                         <p>
-                            FRAGG Investment is a leading impact investment manager and advisor specialized in domestic mobilization and management of funds with a strong focus on impact investment, green economy, and sustainable climate-smart technologies for the Sustainable Development Goals of the United Nations. We support our investments with a triple bottom-line approach – Profit, People, and Planet.
+                        {Data[0].subText}
                         </p>
                     </div>
                 </section>
@@ -46,18 +34,18 @@ const FraggInvestmentManagement = () => {
                     <section className="md:w-1/2  px-[24px] py-[40px] flex items-end bg-[url('/assets/svg/home/investTwo.svg')] text-white">
                         <div>
                             <Goal />
-                            <h3 className="text-xl font-semibold mb-4 pt-[20px]">Our Target</h3>
+                            <h3 className="text-xl font-semibold mb-4 pt-[20px]">{Data[1].title}</h3>
                             <p>
-                                FRAGG Investment is keen on investing in high impact companies in Africa, Latin America, and the Caribbean, with a special focus on sectors such as renewable energy, sustainable agriculture, and financial inclusion. We aim to support projects that have a positive social and environmental impact on their local communities and the wider world.
+                            {Data[1].subText}
                             </p>
                         </div>
                     </section>
                     <section className="md:w-1/2 px-[24px] py-[40px] flex items-end bg-[url('/assets/svg/home/investThree.svg')] text-white">
                         <div>
                             <Earth />
-                            <h3 className="text-xl font-semibold mb-4 pt-[20px]">Our Team</h3>
+                            <h3 className="text-xl font-semibold mb-4 pt-[20px]">{Data[2].title}</h3>
                             <p>
-                                With our international team of investment professionals and advisors, we provide extensive networks that span across Africa, Latin America, and the Caribbean. Our team includes recognized leaders in green economy and development finance with a strong commitment to achieving impact-driven results. We work closely with clients to tailor strategies that meet their specific investment goals and objectives.
+                            {Data[2].subText}
                             </p>
                         </div>
                     </section>
