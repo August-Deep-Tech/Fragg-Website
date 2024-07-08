f845b1a7c9ef9839cbc810342fd4510c331e054c
Jaachi Okafor <okaforjaachi@gmail.com>
2024-07-08 13:54:49 +0100

    changed homepage hamburger icon color, corrected size of map on homepage and set favicon svg

--- src/app/fragg-nigeria/page.tsx
+++ src/app/fragg-nigeria/page.tsx
@@ -1,19 +1,19 @@
-import BecomeanInvestor from '@/pageSections/BecomeanInvestor'
-import ConnectionSection from '@/pageSections/ConnectionSection'
-import FraggInvestmentManagement from '@/pageSections/FraggInvestment'
-import Group from '@/pageSections/Group'
-import GroupContact from '@/pageSections/GroupContact'
-import GrowWithFragg from '@/pageSections/GrowWithFragg'
-import HeroSection from '@/pageSections/HeroSection'
-import OurStats from '@/pageSections/OurStats'
-import PriorityStrategy from '@/pageSections/PriorityStrategy'
-import WhatWeOffer from '@/pageSections/WhatWeOffer'
-import React from 'react'
+import BecomeanInvestor from "@/pageSections/BecomeanInvestor";
+import ConnectionSection from "@/pageSections/ConnectionSection";
+import FraggInvestmentManagement from "@/pageSections/FraggInvestment";
+import Group from "@/pageSections/Group";
+import GroupContact from "@/pageSections/GroupContact";
+import GrowWithFragg from "@/pageSections/GrowWithFragg";
+import HeroSection from "@/pageSections/HeroSection";
+import OurStats from "@/pageSections/OurStats";
+import PriorityStrategy from "@/pageSections/PriorityStrategy";
+import WhatWeOffer from "@/pageSections/WhatWeOffer";
+import React from "react";
 
 const Landing = () => {
   return (
     <div>
-      <HeroSection bgImage={'home-nigeria'} />
+      <HeroSection bgImage={"home-nigeria"} />
       <ConnectionSection />
       <FraggInvestmentManagement />
       <WhatWeOffer />
@@ -21,10 +21,10 @@ const Landing = () => {
       <OurStats />
       <GrowWithFragg />
       <Group />
-      <GroupContact image="map-nigeria" />
+      <GroupContact />
       <BecomeanInvestor />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
